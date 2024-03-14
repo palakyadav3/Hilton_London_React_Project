5a100bced14fe77ecb4f4c40000a246ca68dedb8
palakyadav3 <palak38yadav@gmail.com>
2024-03-15 02:12:54 +0530

    Added React-router features

--- App.js
+++ App.js
@@ -3,16 +3,35 @@ import ReactDOM from "react-dom/client";
 import Heading from "./src/components/Header";
 import Footer from "./src/components/Footer";
 import Body from "./src/components/Body";
+import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
+import Contact from "./src/components/Contact";
 
 const AppLayout=()=>{
     return (
-        <div className="bgImage">   
+        <div >   
         <Heading/>
-        <Body/>
+        <Outlet/>
         <Footer/>
         </div>
     )
 }
 
+const appRouter=createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout/>,
+        children:[
+            {
+                path:"/",
+                element:<Body/>
+            },
+            {
+                path:"/Contact ",
+                element:<Contact/>
+            }
+        ]
+    }
+])
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>)
+root.render(<RouterProvider router={appRouter}/>)
