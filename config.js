2fdbc265870d427d3ebb0320864662d352c44ca7
palakyadav3 <palak38yadav@gmail.com>
2024-03-17 15:03:32 +0530

    Constants

---
+++ config.js
@@ -0,0 +1,21 @@
+import Goa from "./assets/img/Goa.jpeg";
+import Rishikesh from "./assets/img/Rishikesh.jpeg";
+
+export const ExploreCard=[
+    {
+        "id":"1",
+        "name":"Goa",
+        "image":Goa,
+        "rating":"4.5",
+        "price":"Rs 30,000.00",
+        "description":"Goa being one of the inevitable tourist destinations in India, it is quite easily accessible from various parts of India by air, rail and road. Snuggled between hills and seas, overlooking the Arabian Sea is graced with some of the India’s best beaches that draw in tourists from distant parts of the country and globe. There is a well knit of roadways that connects Mumbai to Goa. Mumbai to Goa is approximately 600 kilometer, Pune is around 450 kilometer and Kolhapur is approximately 230 kilometers."
+    },
+    {
+        "id":"2",
+        "name":"Rishikesh",
+        "image":Rishikesh,
+        "rating":"4.6",
+        "price":"Rs 20,000.00"
+
+    },
+]
